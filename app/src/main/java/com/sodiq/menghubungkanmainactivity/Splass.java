0d20103a842413de08101e62fbff85dbb70a7aa3
moh fajar sodiq <moh.fajar229@gmail.com>
2019-10-25 15:41:14 +0700

    menambahlayoutdan splashscreen

---
+++ app/src/main/java/com/sodiq/menghubungkanmainactivity/Splass.java
@@ -0,0 +1,13 @@
+package com.sodiq.menghubungkanmainactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splass2);
+    }
+}
